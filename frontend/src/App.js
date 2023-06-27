92489b5c6335ecfb263c4d29abc29bb3c1524b3d
Espen Sele <espensele@Sigurds-MacBook-Pro.local>
2023-06-27 13:25:08 +0200

    Adjust content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Data from "./pages/Data";
-import Home from "./pages/Home";
 import Game from "./pages/Game";
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Header/>
+      <div style={{paddingTop: "4em"}}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/data" element={<Data />} />
           <Route path="/game" element={<Game />} />
         </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
