6f9e89bf24e1bc4074441364a690f0dd8c9dc9f6
minawoien <mc.woien@stud.uis.no>
2023-06-27 11:28:58 +0200

    created a file for view of chessboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Data from "./pages/Data";
 import Home from "./pages/Home";
+import Game from "./pages/Game";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/data" element={<Data />} />
+          <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Game/index.jsx
@@ -0,0 +1,7 @@
+export default function Game() {
+  return (
+    <div>
+      <h1>Game</h1>
+    </div>
+  );
+}
