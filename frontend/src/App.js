81d6bd0902ae35924a786b0b7472393498dc602f
Espen Sele <espensele@Sigurds-MacBook-Pro.local>
2023-06-27 11:35:23 +0200

    Added template header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
 import Data from "./pages/Data";
 import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/data" element={<Data />} />
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,74 @@
+import { Avatar, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const navigate = useNavigate();
+  return (
+    <Box
+      id="main-container"
+      sx={{
+        
+        position: "absolute",
+        backgroundColor: "white",
+        width: "100%",
+        height: "4em",
+        
+        display: "flex",
+        justifyContent: "stretch",
+        borderBottom: "1px solid black",
+        zIndex: 10,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "left",
+          width: "80%",
+          height: "90%",
+          paddingLeft: "2%",
+        }}
+      >
+        <Button
+          
+          onClick={() => navigate('')}
+          sx={{
+            width: "10em",
+            height: "auto",
+          }}
+          startIcon={
+            <Avatar
+              src="https://www.miles.no/wp-content/uploads/2020/11/miles_logo_red_rgb.jpg"
+              alt="Miles logo"
+              variant="square"
+              sx={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          }
+        ></Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "right",
+          width: "80%",
+          paddingRight: "2%",
+        }}
+      >
+        <Button
+          onClick={() => navigate("")}
+          sx={{
+            width: "3em",
+            height: "100%",
+            borderRadius: "50%",
+          }}
+        >
+          <Avatar sx={{ width: "80%", bgcolor: "black" }} />
+        </Button>
+      </Box>
+    </Box>
+  );
+}
