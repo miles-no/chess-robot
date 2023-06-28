975caf2750669604bf4d701e9e45c496750a66d4
Espen Sele <espensele@Sigurds-MacBook-Pro.local>
2023-06-28 08:30:49 +0200

    Coordinates x-axis

--- frontend/src/Components/Header/Tile.jsx
+++ frontend/src/Components/Header/Tile.jsx
@@ -1,18 +1,28 @@
 import "./Tile.css";
 
 export default function Tile(props) {
-  console.log(props.number);
   if (props.number % 2 === 0) {
     return (
+      <div>
       <div className="tile white-tile">
         <img src={props.image} alt=" " />
       </div>
+            <div className="tile coordinate">
+            <h1>{props.coordinatesX}</h1> 
+        </div>
+
+        </div>
     );
   } else {
     return (
+      <div>
       <div className="tile black-tile">
         <img src={props.image} alt=" " />
       </div>
+            <div className="tile coordinate">
+            <h1>{props.coordinatesX}</h1> 
+        </div>
+        </div>
     );
   }
 }
