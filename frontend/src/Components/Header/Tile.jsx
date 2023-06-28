fb41cce0e2cbd5519504324a18fdbd6b44a55ee6
minawoien <mc.woien@stud.uis.no>
2023-06-28 09:28:32 +0200

    small changes

--- frontend/src/Components/Header/Tile.jsx
+++ frontend/src/Components/Header/Tile.jsx
@@ -4,25 +4,34 @@ export default function Tile(props) {
   if (props.number % 2 === 0) {
     return (
       <div>
-      <div className="tile white-tile">
-        <img src={props.image} alt=" " />
+        <div className="tile white-tile">
+          {props.image && (
+            <div
+              className="chess-piece"
+              style={{ backgroundImage: `url(${props.image})` }}
+            ></div>
+          )}
+        </div>
+        <div className="tile coordinate">
+          <h1>{props.coordinatesX}</h1>
+        </div>
       </div>
-            <div className="tile coordinate">
-            <h1>{props.coordinatesX}</h1> 
-        </div>
-
-        </div>
     );
   } else {
     return (
       <div>
-      <div className="tile black-tile">
-        <img src={props.image} alt=" " />
+        <div className="tile black-tile">
+          {props.image && (
+            <div
+              className="chess-piece"
+              style={{ backgroundImage: `url(${props.image})` }}
+            ></div>
+          )}
+        </div>
+        <div className="tile coordinate">
+          <h1>{props.coordinatesX}</h1>
+        </div>
       </div>
-            <div className="tile coordinate">
-            <h1>{props.coordinatesX}</h1> 
-        </div>
-        </div>
     );
   }
 }
