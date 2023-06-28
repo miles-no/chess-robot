d6b67fe2f42bf0448a5bbb1f967d1f56689eba78
minawoien <mc.woien@stud.uis.no>
2023-06-28 07:58:37 +0200

    Added the remaining pieces

--- frontend/src/Components/Header/Chessboard.jsx
+++ frontend/src/Components/Header/Chessboard.jsx
@@ -6,6 +6,20 @@ export default function Chessboard() {
 
   const pieces = [];
 
+  for (let p = 0; p < 2; p++) {
+    const type = p === 0 ? "b" : "w";
+    const y = p === 0 ? 7 : 0;
+
+    pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y });
+    pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y });
+    pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y });
+    pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y });
+    pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y });
+    pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y });
+    pieces.push({ image: `assets/images/queen_${type}.png`, x: 3, y });
+    pieces.push({ image: `assets/images/king_${type}.png`, x: 4, y });
+  }
+
   for (let i = 0; i < 8; i++) {
     pieces.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
   }
