18731915b4ba97742af5ee7083b47a637facbe96
minawoien <mc.woien@stud.uis.no>
2023-06-30 08:08:34 +0200

    created a component for leaderboard

--- frontend/src/pages/Leaderboard/index.jsx
+++ frontend/src/pages/Leaderboard/index.jsx
@@ -1,3 +1,10 @@
+import LeaderboardTable from "../../Components/Leaderboard/LeaderboardTable";
+
 export default function Leaderboard() {
-  return <h1>Leaderboard</h1>;
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <LeaderboardTable />
+    </div>
+  );
 }
