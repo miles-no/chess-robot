d0e0d1bc85d9acedaebf92010db89bf2dbc36f22
minawoien <mc.woien@stud.uis.no>
2023-06-30 11:12:52 +0200

    Translator match stockfish format

--- backend/translator.py
+++ backend/translator.py
@@ -9,11 +9,12 @@ translator = {
     "h": 7
 }
 
-def translate_notation(prevState, currState):
-    prevX, prevY = prevState[:len(prevState)//2].lower(), prevState[len(prevState)//2:]
-    currX, currY = currState[:len(currState)//2].lower(), currState[len(currState)//2:]
+def translate_notation(fen):
+    prevState, currState = fen[:len(fen)//2], fen[len(fen)//2:]
+    prevX, prevY = prevState[:len(prevState)//2], prevState[len(prevState)//2:]
+    currX, currY = currState[:len(currState)//2], currState[len(currState)//2:]
     return(translator[prevX], int(prevY)-1, translator[currX], int(currY)-1)
 
 
 if __name__ == "__main__":
-    print(translate_notation("E2", "a2"))
+    print(translate_notation("e2a2"))
