413a137edf4cb1414059e2b806a140b0e8bbbaae
minawoien <mc.woien@stud.uis.no>
2023-07-03 12:49:08 +0200

    Added stockfish for chess logic

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_socketio import SocketIO
 import datetime
 from translator import translate_notation
+from chessLogic.chess import ChessLogic
+import chess
 
 x = datetime.datetime.now()
 
 app = Flask(__name__)
 socket_io = SocketIO(app, cors_allowed_origins="*")
+
+chess_logic = ChessLogic()
 
 # SocketIO to handle new connections
 # Prints for every new connection
@@ -18,7 +22,7 @@ def handle_connect():
 @socket_io.on('to-server')
 def handle_to_server(arg):
     print(f'new to-server event: {arg}')
-    message =  getUserInput()
+    message = getStockfishMove() #! Change for user input
     messageDictionary = {"prevX": message[0], "prevY": message[1], "nextX": message[2], "nextY": message[3]}
     socket_io.emit('from-server', messageDictionary)
 
@@ -27,6 +31,12 @@ def getUserInput():
     move = input("Enter move: ")
     return translate_notation(move)
 
+
+def getStockfishMove():
+    board = chess.Board()
+    move = chess_logic.getBestMove(board)
+    message = translate_notation(move.uci())
+    return message
 
 if __name__ == '__main__':
     socket_io.run(app, port=5000)
