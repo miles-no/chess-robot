bbc5539a9407520d3b90566dce0ac4c49bc8b6fa
minawoien <mc.woien@stud.uis.no>
2023-07-04 09:16:06 +0200

    Reset board for new game, move pieces on backend board

--- backend/chessLogic/chessLogic.py
+++ backend/chessLogic/chessLogic.py
@@ -3,23 +3,42 @@ import chess.engine
 
 class ChessLogic:
     def __init__(self):
+        self.board = chess.Board()
         self.engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/opt/stockfish/bin/stockfish")
 
-    def getBestMove(self, board):
-        result = self.engine.play(board, chess.engine.Limit(time=0.1))
+    # Returns move in 'chess.Move' format
+    def getBestMove(self):
+        result = self.engine.play(self.board, chess.engine.Limit(time=0.1))
         best_move = result.move
         return best_move
     
-    def quitEnine(self):
+    def quitEngine(self):
         self.engine.quit()
     
-    def validateMove(self, board, move):
-        return move in board.legal_moves
+    # Takes move in 'chess.Move' format
+    def validateMove(self, move):
+        return move in self.board.legal_moves
+    
+    # Takes move in string format
+    def movePiece(self, move):
+        move = chess.Move.from_uci(move)
+        self.board.push(move)
+        # return board
+    
+    def get_board(self):
+        return self.board
+    
+    def reset_board(self):
+        self.board = chess.Board()
 
 if __name__ == "__main__":
-    board = chess.Board()
-    print(board)
-    print(type(board))
     chessLogic = ChessLogic()
-    print(chessLogic.getBestMove(board))
-    chessLogic.quitEnine()
+    for i in range(5):
+        print(chessLogic.get_board())
+        move = str(chessLogic.getBestMove())
+        print(move)
+        chessLogic.movePiece(move)
+        print(chessLogic.get_board())
+    chessLogic.reset_board()
+    print(chessLogic.get_board())
+    chessLogic.quitEngine()
