da702cfe2f79f9b51d7e7fcda0621f00e41f6f46
minawoien <mc.woien@stud.uis.no>
2023-07-05 13:09:13 +0200

    cleanup server and chessLogic

--- backend/server.py
+++ backend/server.py
@@ -20,28 +20,6 @@ chess_logic = ChessLogic(STOCKFISH_PATH)
 def handle_connect():
     print('new connection')
 
-# Will print the sent argument amd semd back the current time
-@socket_io.on('to-server')
-def handle_to_server(arg):
-    print(f'new to-server event: {arg}')
-    move = getStockfishMove() #! Change for user input
-    validation = chess_logic.validateMove(move)
-    if not validation:
-        print("Stockfish made an illegal move")
-        print("MOVE ::: " + move)
-        socket_io.emit('from-server', validation)
-        return
-    print("Stockfish made a legal move")
-    move = move.uci()
-    chess_logic.movePiece(move)
-    message = translate_notation(move)
-    messageDictionary = {"prevX": message[0], "prevY": message[1], 
-                         "nextX": message[2], "nextY": message[3], "checkmate": chess_logic.check_mate(), "result": chess_logic.getOutcome()}
-    print(chess_logic.get_board())
-    if(chess_logic.getOutcome()):
-        print(chess_logic.getOutcome())
-    socket_io.emit('from-server', messageDictionary)
-
 @socket_io.on('new-game')
 def newGame(arg):
     print(f'new to-server event: {arg}')
@@ -49,43 +27,37 @@ def newGame(arg):
 
 @socket_io.on('start-game')
 def startGame(arg):
-    print(f'new to-server event: {arg}')
     while chess_logic.getOutcome() is None:
         move = getStockfishMove() #! Change for user input
         validation = chess_logic.validateMove(move)
-        if not validation:
-            print("Illegal move")
+        if not chess_logic.validateMove(move):
             socket_io.emit('from-server', validation)
             return
-        print("Stockfish made a legal move")
         move = move.uci()
         chess_logic.movePiece(move)
         if chess_logic.checkSpecialMove()[0] == "castling":
             rookMove = chess_logic.checkSpecialMove()[1]
-            message2 = translate_notation(rookMove)
-            messageDictionary2 = {"prevX": message2[0], "prevY": message2[1], 
-                            "nextX": message2[2], "nextY": message2[3],
-                              "checkmate": chess_logic.check_mate(), "result": chess_logic.getOutcome()}
-            socket_io.emit('from-server', messageDictionary2)
-            print(chess_logic.get_board())
+            print("castling")
+            emitMove(rookMove)
             time.sleep(1) #Sleep added because the frontend is unable to keep up with rook special move
         elif chess_logic.checkSpecialMove()[0]=="passant":
             pass
         elif chess_logic.checkSpecialMove()[0]=="promotion":
             promotion = move[-1]
             move = move[:-1]
-        message = translate_notation(move)
-        messageDictionary = {"prevX": message[0], "prevY": message[1], 
-                            "nextX": message[2], "nextY": message[3], "checkmate": chess_logic.check_mate(), "result": chess_logic.getOutcome()}
-        print(chess_logic.get_board())
-        if(chess_logic.getOutcome()):
-            print(chess_logic.getOutcome())
-            print("Result, winner is: " + chess_logic.getWinner())
-        socket_io.emit('from-server', messageDictionary)
-        if chess_logic.checkSpecialMove()[0]=="promotion":
+            print("promotion")
             socket_io.emit('promotion', promotion)
-    print("Game over")
+        emitMove(move)
+        print(chess_logic.get_board())
+    print(chess_logic.get_board())
+    print(chess_logic.getOutcome())
+    print("Result, winner is: " + chess_logic.getWinner())
 
+def emitMove(move):
+    tMove = translate_notation(move)
+    msg = {"prevX": tMove[0], "prevY": tMove[1], "nextX": tMove[2], "nextY": tMove[3], 
+           "checkmate": chess_logic.check_mate(), "result": chess_logic.getOutcome()}
+    socket_io.emit('from-server', msg)
 
 def getUserInput():
     move = input("Enter move: ")
