0f7f5aa019e2bea29730f8828e5511faa7817bac
minawoien <mc.woien@stud.uis.no>
2023-07-07 08:34:46 +0200

    Moved special moves into separate functions

--- backend/server.py
+++ backend/server.py
@@ -37,22 +37,19 @@ def startGame(arg):
             tmove = translate_notation(move.uci())
             passant = {"currX": tmove[2], "currY": tmove[1]}
             socket_io.emit('passant', passant)
-            print("Passant")
         move = move.uci()
         chess_logic.movePiece(move)
-        if chess_logic.checkSpecialMove()[0] == "castling":
-            rookMove = chess_logic.checkSpecialMove()[1]
-            print("castling")
+        if chess_logic.checkCastling():
+            rookMove = chess_logic.checkCastling()
             emitMove(rookMove)
             time.sleep(1) #Sleep added because the frontend is unable to keep up with rook special move
-        elif chess_logic.checkSpecialMove()[0]=="promotion":
+        elif chess_logic.checkPromotion():
             promotion = move[-1]
             move = move[:-1]
-        emitMove(move)
-        if chess_logic.checkSpecialMove()[0]=="promotion":
-            move = translate_notation(move)
-            promo = {"promotion": promotion, "currX": move[0], "currY": move[1], "turn": playerTurn}
+            pmove = translate_notation(move)
+            promo = {"promotion": promotion, "currX": pmove[0], "currY": pmove[1], "turn": playerTurn}
             socket_io.emit('promotion', promo)
+        emitMove(move)
         print(chess_logic.get_board())
     print(chess_logic.getOutcome())
     print("Result, winner is: " + chess_logic.getWinner())
@@ -72,6 +69,41 @@ def getStockfishMove():
 
 
 # PASSANT_TEST = ["f2f3", "b7b5", "g2g4", "b5b4", "a2a4", "b4a3", "h2h4", "h7h6"]
+# PROMOTION_TEST = ["a2a4", "b7b5", "a4b5", "b8c6", "a1a5", "c6a5", "b5b6", "a5b3", "b6b7", "b3a1", "b7b8r", "g7g5"]
+
+# @socket_io.on('start-game')
+# def startGame(arg):
+#     for i in range(len(PROMOTION_TEST)-1):
+#         playerTurn = chess_logic.getPlayerTurn()
+#         move = PROMOTION_TEST[i]
+#         move = chess.Move.from_uci(move)
+#         if not chess_logic.validateMove(move):
+#             socket_io.emit('validation-error')
+#             continue
+#         if chess_logic.checkPassant(move):
+#             tmove = translate_notation(move.uci())
+#             passant = {"currX": tmove[2], "currY": tmove[1]}
+#             socket_io.emit('passant', passant)
+#             print("Passant")
+#         move = move.uci()
+#         chess_logic.movePiece(move)
+#         if chess_logic.checkSpecialMove():
+#             rookMove = chess_logic.checkSpecialMove()
+#             print("castling")
+#             emitMove(rookMove)
+#             time.sleep(1) #Sleep added because the frontend is unable to keep up with rook special move
+#         elif chess_logic.checkPromotion():
+#             promotion = move[-1]
+#             move = move[:-1]
+#             print("SECOND IF")
+#             pmove = translate_notation(move)
+#             promo = {"promotion": promotion, "currX": pmove[0], "currY": pmove[1], "turn": playerTurn}
+#             socket_io.emit('promotion', promo)
+#         emitMove(move)
+#         print(chess_logic.get_board())
+#         time.sleep(1)
+#     print("Game over")
+
 
 # @socket_io.on('start-game')
 # def testPassant(arg):
