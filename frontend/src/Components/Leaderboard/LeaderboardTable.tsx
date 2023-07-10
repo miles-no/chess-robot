1513297d019141fbf91ae22e39489360ba5023c5
Espen Sele <espensele@Sigurds-MacBook-Pro.local>
2023-07-10 09:15:22 +0200

    Converted rest of files

--- frontend/src/Components/Leaderboard/LeaderboardTable.tsx
+++ frontend/src/Components/Leaderboard/LeaderboardTable.tsx
@@ -1,12 +1,12 @@
-import "./LeaderboardTable.css";
-import * as React from "react";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import * as React from "react";
+import "./LeaderboardTable.css";
 
 export default function LeaderboardTable() {
   const data = [
