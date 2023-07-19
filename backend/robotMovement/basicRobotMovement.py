65e6ae8b2073a14fa320adca31411a0740336493
minawoien <mc.woien@stud.uis.no>
2023-07-19 11:17:48 +0200

    Add correct heights and tested moving pieces

--- backend/robotMovement/basicRobotMovement.py
+++ backend/robotMovement/basicRobotMovement.py
@@ -5,10 +5,13 @@ import time
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
 
 from xarm.wrapper import XArmAPI
+from chessRobot import ChessRobot
 from configparser import ConfigParser
 parser = ConfigParser()
 parser.read('robot.conf')
 ip = parser.get('xArm', 'ip')
+
+cr = ChessRobot()
 
 #Initialize robot
 arm = XArmAPI(ip)
@@ -19,32 +22,53 @@ time.sleep(2)
 #Reset to start position(Zero position)
 arm.reset(wait=True)
 
-#Move robot
-arm.set_position(x=300, y=100, z=100, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
-print(arm.get_position(), arm.get_position(is_radian=True))
-arm.set_position(x=300, y=100, z=43, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
-print(arm.get_position(), arm.get_position(is_radian=True))
+piece = 5
+print(cr.piece_height[piece])
+
+# #Move robot
+# arm.set_position(x=300, y=100, z=100, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
+# print(arm.get_position(), arm.get_position(is_radian=True))
+# arm.set_position(x=300, y=100, z=cr.piece_height[piece], roll=-180, pitch=0, yaw=0, speed=100, wait=True) #43 -king 65
+# print(arm.get_position(), arm.get_position(is_radian=True))
+# time.sleep(1)
+# #Close gripper
+# arm.close_lite6_gripper()
+# time.sleep(2)
+# arm.set_position(x=300, y=100, z=200, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
+# print(arm.get_position(), arm.get_position(is_radian=True))
+# arm.set_position(x=300, y=0, z=cr.piece_height[piece], roll=-180, pitch=0, yaw=0, speed=100, wait=True)
+# print(arm.get_position(), arm.get_position(is_radian=True))
+# time.sleep(2)
+# #Open gripper
+# arm.open_lite6_gripper()
+# time.sleep(1)
+# #Stop gripper engine
+# arm.stop_lite6_gripper()
+# time.sleep(1)
+# #Inclination before going back to starting position
+# arm.set_position(x=100, y=0, z=200, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
+# print(arm.get_position(), arm.get_position(is_radian=True))
+# time.sleep(1)
+# # #Go back to start position
+# arm.reset(wait=True)
+# arm.disconnect()
+
+arm.set_position(x=150, y=116, z=200, roll=-180, pitch=0, yaw=0, speed=50, wait=True)
 time.sleep(1)
-#Close gripper
+arm.set_position(x=150, y=116, z=cr.piece_height[piece], roll=-180, pitch=0, yaw=0, speed=50, wait=True)
+time.sleep(1)
 arm.close_lite6_gripper()
-time.sleep(2)
-arm.set_position(x=300, y=100, z=200, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
-print(arm.get_position(), arm.get_position(is_radian=True))
-arm.set_position(x=300, y=0, z=43, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
-print(arm.get_position(), arm.get_position(is_radian=True))
-time.sleep(2)
-#Open gripper
+time.sleep(1)
+arm.set_position(x=150, y=116, z=150, roll=-180, pitch=0, yaw=0, speed=50, wait=True)
+time.sleep(1)
+arm.set_position(x=186, y=80, z=150, roll=-180, pitch=0, yaw=0, speed=50, wait=True) #36 en rute frem
+time.sleep(1)
+arm.set_position(x=186, y=80, z=cr.piece_height[piece], roll=-180, pitch=0, yaw=0, speed=50, wait=True)
+time.sleep(1)
 arm.open_lite6_gripper()
 time.sleep(1)
-#Stop gripper engine
 arm.stop_lite6_gripper()
-time.sleep(1)
-#Inclination before going back to starting position
-arm.set_position(x=100, y=0, z=200, roll=-180, pitch=0, yaw=0, speed=100, wait=True)
-print(arm.get_position(), arm.get_position(is_radian=True))
-time.sleep(1)
-#Go back to start position
-arm.reset(wait=True)
+#arm.reset(wait=True)
 arm.disconnect()
 
 
