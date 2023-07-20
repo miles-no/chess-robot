219577291fd3b37c53399cf833d8841acec94d3a
minawoien <mc.woien@stud.uis.no>
2023-07-20 09:34:58 +0200

    change speed and move pieces to other side when 6 is taken

--- backend/robotMovement/chessRobot.py
+++ backend/robotMovement/chessRobot.py
@@ -28,15 +28,15 @@ class ChessRobot:
         self.arm.reset(wait=True)
 
     def movePiece(self, start_x, start_y, x, y, piece):
-        self.moving(100, 0, 150)
-        self.moving(start_x, start_y, 150)
+        self.moving(100, 0, 150, 80)
+        self.moving(start_x, start_y, 150, 80)
         self.moving(start_x, start_y, self.piece_height[piece])
         
         self.arm.close_lite6_gripper()
         time.sleep(1)
 
         self.moving(start_x, start_y, 150)
-        self.moving(x, y, 150)
+        self.moving(x, y, 150, 80)
         self.moving(x, y, self.piece_height[piece])
     
         self.arm.open_lite6_gripper()
@@ -45,7 +45,7 @@ class ChessRobot:
         time.sleep(1)
 
         self.moving(x, y, 150)
-        self.moving(100, 0, 150)
+        self.moving(100, 0, 150, 80)
 
     def doMove(self, move, piece):
         cc = ChessCoordinates()
@@ -55,19 +55,25 @@ class ChessRobot:
         self.movePiece(x_from, y_from, x_to, y_to, piece)
         self.arm.reset(wait=True)
     
-    def moving(self, x, y, z):
-        self.arm.set_position(x=x, y=y, z=z, roll=-180, pitch=0, yaw=0,speed=50, wait=True)
+    def moving(self, x, y, z, speed=50):
+        self.arm.set_position(x=x, y=y, z=z, roll=-180, pitch=0, yaw=0,speed=speed, wait=True)
         time.sleep(1)
     
     def disconnect(self):
         self.arm.disconnect()
 
+    # Move the piece that have been taken outside 
     def move_taken(self, move_from, piece):
         cc = ChessCoordinates()
         x_from, y_from = cc.chess_to_robot(move_from)
         self.taken.append(piece)
-        print(self.taken)
-        self.movePiece(x_from, y_from, 130+len(self.taken)*40, 160, piece)
+        if len(self.taken) < 7:
+            x = 130 + (len(self.taken)-1)*40
+            y = 160
+        else:
+            x = 130 + (len(self.taken)-7)*40
+            y = -162
+        self.movePiece(x_from, y_from, x, y, piece)
 
 
 if __name__ == "__main__":
