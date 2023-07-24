937c00b09ec08ad7efea560b0e4f7e97617fa9f4
minawoien <mc.woien@stud.uis.no>
2023-07-24 15:22:54 +0200

    trying to avoid error collision

--- backend/robotMovement/chessRobot.py
+++ backend/robotMovement/chessRobot.py
@@ -1,7 +1,8 @@
+import math
 import time
 from xarm.wrapper import XArmAPI
 from configparser import ConfigParser
-from robotMovement.chessCoordinates import ChessCoordinates
+from chessCoordinates import ChessCoordinates
 
 class ChessRobot:
     def __init__(self):
@@ -14,7 +15,7 @@ class ChessRobot:
             "k": 67 #king
         }
         self.parser = ConfigParser()
-        self.parser.read('robotMovement/robot.conf')
+        self.parser.read('robot.conf')
         self.arm = XArmAPI(self.parser.get('xArm', 'ip'))
         self.initialize()
         self.taken = []
@@ -23,13 +24,16 @@ class ChessRobot:
         self.arm.motion_enable(enable=True)
         self.arm.set_mode(0)
         self.arm.set_state(state=0)
-        self.arm.set_collision_sensitivity(3)
+        self.arm.set_collision_sensitivity(5)
         time.sleep(2)
         self.arm.reset(wait=True)
 
-    def movePiece(self, start_x, start_y, x, y, piece):
+    def movePiece(self, start_x, start_y, x, y, piece, king_position=False):
         self.moving(100, 0, 150, 100)
         self.moving(start_x, start_y, 150, 100)
+        if king_position:
+            print("King on the side")
+            self.rotate_robot()
         self.moving(start_x, start_y, self.piece_height[piece])
         
         time.sleep(1)
@@ -47,13 +51,23 @@ class ChessRobot:
         self.moving(x, y, 150, 80)
         self.moving(100, 0, 150, 100)
 
-    def doMove(self, move, piece):
+    def doMove(self, move, piece, king_position=False):
         cc = ChessCoordinates()
         move_from, move_to = move[:len(move)//2], move[len(move)//2:]
-        x_from, y_from = cc.chess_to_robot(move_from)
-        x_to, y_to = cc.chess_to_robot(move_to)
-        self.movePiece(x_from, y_from, x_to, y_to, piece)
-        self.arm.reset(wait=True)
+        start_x, start_y = cc.chess_to_robot(move_from)
+        #x_to, y_to = cc.chess_to_robot(move_to)
+        self.arm.set_position(x=100, y=0, z=150, roll=-180, pitch=0, yaw=0,speed=100, wait=True)
+        self.arm.set_position(x=start_x, y=start_y, z=150, roll=-180, pitch=0, yaw=0,speed=100, wait=True)
+        self.arm.set_position(x=start_x, y=start_y, z=150, roll=-180, pitch=0, yaw=0,speed=50, wait=True)
+
+
+        #self.arm.set_servo_angle(servo_id=6, angle=90, is_radian=False, wait=True)
+ 
+        self.arm.set_position(x=start_x, y=start_y, z=self.piece_height[piece], roll=-180, pitch=0, yaw=0, speed=80, wait=True)
+    
+
+        # self.movePiece(x_from, y_from, x_to, y_to, piece, king_position)
+        # self.arm.reset(wait=True)
     
     def moving(self, x, y, z, speed=50):
         time.sleep(1)
@@ -77,15 +91,16 @@ class ChessRobot:
             x = 130 + (len(self.taken)-7)*40
             y = -162
         self.movePiece(x_from, y_from, x, y, piece)
+    
+    def rotate_robot(self):
+        self.arm.set_servo_angle(servo_id=6, angle=90, is_radian=False, wait=False)
+        # self.arm.set_position(x=200, y=0, z=200, roll=-180, pitch=0, yaw=0,speed=80, wait=True
 
 
 if __name__ == "__main__":
     cr = ChessRobot()
-    cr.doMove("a1a3", "p")
-    cr.doMove("a3e4", "p")
-    cr.doMove("e4g6", "p")
-    cr.doMove("g6a1", "p")
-
+    #cr.doMove("e2e4", "p")
+    cr.doMove("f1d3", "b", True)
     
 
 
