baa2eb52337c8cf57954fb4190a25aa07765aec8
minawoien <mc.woien@stud.uis.no>
2023-07-25 15:16:06 +0200

    move king so knight can be moved

--- backend/robotMovement/chessRobot.py
+++ backend/robotMovement/chessRobot.py
@@ -35,6 +35,15 @@ class ChessRobot:
         if self.cc.kq_on_side_lift(kq_position, move):
             self.moving(start_x, start_y, 150, yaw=90)
             self.moving(start_x, start_y, self.piece_height[piece], 80, yaw=90)
+            if piece == "n":
+                print("Knight")
+                self.arm.close_lite6_gripper()
+                self.moving(start_x, start_y, 50, 80, yaw=0)
+                self.moving(start_x, start_y, 100, 80, yaw=90)
+                self.arm.open_lite6_gripper()
+                time.sleep(1)
+                self.arm.stop_lite6_gripper()
+                self.moving(start_x, start_y, self.piece_height[piece], 80, yaw=90)
         else:
             self.moving(start_x, start_y, 150)
             self.moving(start_x, start_y, self.piece_height[piece], 80)
@@ -44,16 +53,22 @@ class ChessRobot:
         self.moving(start_x, start_y, 150)
 
         if self.cc.kq_on_side_down(kq_position, move):
-            self.moving(x, y, 150, yaw=90)
-            self.moving(x, y, self.piece_height[piece], 80, yaw=90)
+            self.moving(x, y, 150, yaw=-90)
+            self.moving(x, y, self.piece_height[piece], 80, yaw=-90)
+            if piece == "n":
+                self.moving(x, y, self.piece_height[piece], 80)
+                self.arm.open_lite6_gripper()
+                time.sleep(1)
+                self.arm.stop_lite6_gripper()
+                self.moving(x, y, 150, 80)
+                self.arm.close_lite6_gripper()
         else:
             self.moving(x, y, 150)
             self.moving(x, y, self.piece_height[piece], 80)
-
+        
         self.arm.open_lite6_gripper()
         time.sleep(1)
         self.arm.stop_lite6_gripper()
-
         self.moving(x, y, 150)
         self.moving(100, 0, 150)
 
@@ -61,7 +76,22 @@ class ChessRobot:
         move_from, move_to = move[:len(move)//2], move[len(move)//2:]
         x_from, y_from = self.cc.chess_to_robot(move_from)
         x_to, y_to = self.cc.chess_to_robot(move_to)
-        self.movePiece(x_from, y_from, x_to, y_to, piece, move, kq_position)
+        if kq_position != None:
+            kq = ""
+            if self.cc.kq_on_side_lift(kq_position, move):
+                _, kq = self.cc.kq_on_side_lift(kq_position, move)
+            if self.cc.kq_on_side_down(kq_position, move):
+                _, kq = self.cc.kq_on_side_down(kq_position, move)
+            if piece == "n" and kq == "w_king" or piece == "n" and kq == "b_king":
+                x_king, y_king = self.cc.chess_to_robot(kq_position[kq])
+                self.movePiece(x_king, y_king, 110, 0, "k", move)
+                self.movePiece(x_from, y_from, x_to, y_to, piece, move, kq_position)
+                self.movePiece(110, 0, x_king, y_king, "k", move)
+            else:
+                self.movePiece(x_from, y_from, x_to, y_to, piece, move, kq_position)
+        else:
+            self.movePiece(x_from, y_from, x_to, y_to, piece, move, kq_position)
+
     
     def moving(self, x, y, z, speed=100, yaw=0):
         self.arm.set_position(x=x, y=y, z=z, roll=-180, pitch=0, yaw=yaw, speed=speed, wait=True)
@@ -87,5 +117,5 @@ class ChessRobot:
 
 if __name__ == "__main__":
     cr = ChessRobot()
-    cr.doMove("c1a3", "b", ["e1","d1"])
-    cr.doMove("a3c1", "b", ["e1","d1"])
+    cr.doMove("b1b5", "n", {"w_king":"c5", "w_queen":"c1", "b_king":"e8"})
+
