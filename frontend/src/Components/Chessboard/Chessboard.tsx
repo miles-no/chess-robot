2af3ab8685d6d27f3bf359e7b713aadd0de01485
minawoien <mc.woien@stud.uis.no>
2023-07-27 21:23:26 +0200

    scale game page

--- frontend/src/Components/Chessboard/Chessboard.tsx
+++ frontend/src/Components/Chessboard/Chessboard.tsx
@@ -1,10 +1,9 @@
 import { Chessboard } from "react-chessboard";
 import { Socket } from "socket.io-client";
 interface mychessboardProps {
-  boardWidth: number;
   FEN: string;
   socket: Socket;
 }
 export default function MyChessboard(props: mychessboardProps) {
-  return <Chessboard boardWidth={props.boardWidth} position={props.FEN} />;
+  return <Chessboard position={props.FEN} />;
 }
