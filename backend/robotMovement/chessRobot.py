31f8817a0099d5fe30a22b0da91cd7c856e1793c
minawoien <mc.woien@stud.uis.no>
2023-08-09 14:15:28 +0200

    added error-handling

--- backend/robotMovement/chessRobot.py
+++ backend/robotMovement/chessRobot.py
@@ -1,7 +1,9 @@
 import time
-from xarm.wrapper import XArmAPI
 from configparser import ConfigParser
+
 from robotMovement.chessCoordinates import ChessCoordinates
+from xarm.wrapper import XArmAPI
+
 
 class ChessRobot:
     def __init__(self):
@@ -9,25 +11,35 @@ class ChessRobot:
         self.parser = ConfigParser()
         self.parser.read('robotMovement/robot.conf')
         self.arm = XArmAPI(self.parser.get('xArm', 'ip'))
-
+        self.taken = []
+        self.cc = ChessCoordinates()
+        self.arm.register_error_warn_changed_callback(callback=self.callback_error_warn_changed)
         # Due to a bug in the xArm SDK, not resetting to the correct zero position upon reset(), 
         # we have to compensate for this by adding the difference between the real zero position and the start position
         self.zero_position = [87, 0, 51.2]
         self.start_position = None
-
-        self.taken = []
-        self.cc = ChessCoordinates()
         self.initialize()
+        
     
+    def callback_error_warn_changed(self, data):
+        if data['error_code'] == 22: # 22 = self collision
+            self.arm.reset(wait=True)
+            self.start_position = self.arm.position
+        if data['error_code'] == 31: # 31 = collision
+            self.arm.motion_enable(enable=True)
+            self.arm.set_mode(0)
+            self.arm.set_state(state=0)
+            self.moving(100, self.arm.position[1], 200)
+
     def initialize(self):
-        print(self.arm.position)
+        self.arm.connect()
         self.arm.motion_enable(enable=True)
         self.arm.set_mode(0)
         self.arm.set_state(state=0)
         self.arm.set_collision_sensitivity(3)
+        time.sleep(1)
         self.arm.reset(wait=True)
         self.start_position = self.arm.position
-        print("Start position: ", self.start_position)
 
     def movePiece(self, start_x, start_y, x, y):
         self.moving(100, 0, 110)
@@ -55,12 +67,15 @@ class ChessRobot:
         x_to, y_to = self.cc.chess_to_robot(move_to)
         self.movePiece(x_from, y_from, x_to, y_to)
 
-    def moving(self, x, y, z, speed=110, yaw=0):
+    def moving(self, x, y, z, speed=110):
         # Calculating the correct position according to physical zero position
-        new_x = x - self.zero_position[0] + self.start_position[0]
-        new_y = y - self.zero_position[1] + self.start_position[1]
-        new_z = z - self.zero_position[2] + self.start_position[2]
-        self.arm.set_position(x=new_x, y=new_y, z=new_z, roll=-180, pitch=0, yaw=yaw, speed=40, wait=True)
+        x_diff = -self.zero_position[0] + self.start_position[0]
+        y_diff = -self.zero_position[1] + self.start_position[1]
+        z_diff = -self.zero_position[2] + self.start_position[2]
+        x = x + x_diff
+        y = y + y_diff
+        z = z + z_diff
+        self.arm.set_position(x, y, z, roll=-180, pitch=0, yaw=0, speed=40, wait=True)
     
     def disconnect(self):
         self.arm.disconnect()
