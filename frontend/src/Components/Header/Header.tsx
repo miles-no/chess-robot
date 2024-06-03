8a02cc18b6dd63e242d6b74cdc2c8f674c79d8db
Henry Sjøen <henry.sjoen@miles.no>
2024-06-03 20:54:11 +0200

    Use vite instead of deprecated create-react-app

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,89 +1,84 @@
-import { Avatar, Button } from "@mui/material";
+import {Avatar, Button} from "@mui/material";
 import Box from "@mui/material/Box";
-import { useNavigate } from "react-router-dom";
-import { Socket } from "socket.io-client";
-import { GameState, useGameContext } from "../../pages/Game/GameContext";
-import { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import {GameState, useGameContext} from "../../pages/Game/GameContext";
+import {useState} from "react";
 
-interface headerProps {
-  socket: Socket;
-}
+export default function Header() {
+    const navigate = useNavigate();
+    const {gameState} = useGameContext();
+    const [disabled, setDisabled] = useState(false);
+    const handleHome = () => {
+        if (gameState === GameState.inProgress) {
+            setDisabled(true);
+        } else {
+            navigate("");
+        }
+    };
 
-export default function Header(props: headerProps) {
-  const navigate = useNavigate();
-  const { gameState } = useGameContext();
-  const [disabled, setDisabled] = useState(false);
-  const handleHome = () => {
-    if (gameState === GameState.inProgress) {
-      setDisabled(true);
-    } else {
-      navigate("");
-    }
-  };
-
-  return (
-    <Box
-      id="main-container"
-      sx={{
-        position: "absolute",
-        backgroundColor: "white",
-        width: "100%",
-        height: "4em",
-
-        display: "flex",
-        justifyContent: "stretch",
-        borderBottom: "1px solid black",
-        zIndex: 10,
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "left",
-          width: "80%",
-          height: "90%",
-          paddingLeft: "2%",
-        }}
-      >
-        <Button
-          onClick={handleHome}
-          sx={{
-            width: "10em",
-            height: "auto",
-          }}
-          disabled={disabled}
-          startIcon={
-            <Avatar
-              src="https://www.miles.no/wp-content/uploads/2020/11/miles_logo_red_rgb.jpg"
-              alt="Miles logo"
-              variant="square"
-              sx={{
+    return (
+        <Box
+            id="main-container"
+            sx={{
+                position: "absolute",
+                backgroundColor: "white",
                 width: "100%",
-                height: "100%",
-              }}
-            />
-          }
-        ></Button>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "right",
-          width: "80%",
-          paddingRight: "2%",
-        }}
-      >
-        <Button
-          onClick={() => navigate("")}
-          sx={{
-            width: "3em",
-            height: "100%",
-            borderRadius: "50%",
-          }}
+                height: "4em",
+
+                display: "flex",
+                justifyContent: "stretch",
+                borderBottom: "1px solid black",
+                zIndex: 10,
+            }}
         >
-          <Avatar sx={{ width: "80%", bgcolor: "black" }} />
-        </Button>
-      </Box>
-    </Box>
-  );
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "left",
+                    width: "80%",
+                    height: "90%",
+                    paddingLeft: "2%",
+                }}
+            >
+                <Button
+                    onClick={handleHome}
+                    sx={{
+                        width: "10em",
+                        height: "auto",
+                    }}
+                    disabled={disabled}
+                    startIcon={
+                        <Avatar
+                            src="https://www.miles.no/wp-content/uploads/2020/11/miles_logo_red_rgb.jpg"
+                            alt="Miles logo"
+                            variant="square"
+                            sx={{
+                                width: "100%",
+                                height: "100%",
+                            }}
+                        />
+                    }
+                ></Button>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "right",
+                    width: "80%",
+                    paddingRight: "2%",
+                }}
+            >
+                <Button
+                    onClick={() => navigate("")}
+                    sx={{
+                        width: "3em",
+                        height: "100%",
+                        borderRadius: "50%",
+                    }}
+                >
+                    <Avatar sx={{width: "80%", bgcolor: "black"}}/>
+                </Button>
+            </Box>
+        </Box>
+    );
 }
