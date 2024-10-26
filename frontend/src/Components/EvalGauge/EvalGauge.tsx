30c0510183f88954c57d7628d529ef4f5056b104
Daniel Wenner <dwenner@users.noreply.github.com>
2024-10-26 17:04:13 +0200

    Connect arrow to backend socket message

--- frontend/src/Components/EvalGauge/EvalGauge.tsx
+++ frontend/src/Components/EvalGauge/EvalGauge.tsx
@@ -4,10 +4,18 @@ interface EvalGaugeProps {
   score: number
 }
 
-export const EvalGauge = ({score}: EvalGaugeProps) => {
+function clamp(num: number, min: number, max: number) {
+  return num <= min
+    ? min
+    : num >= max
+      ? max
+      : num
+}
 
-  // TODO calculate gaugePosition from score
-  const gaugePosition = 20;
+export const EvalGauge = ({ score }: EvalGaugeProps) => {
+
+  const nomalized = (((score ?? 0) + 1000) / 2000) * 100
+  const gaugePosition = clamp(nomalized, 0, 100);
 
   return (
     <div style={{
@@ -17,19 +25,20 @@ export const EvalGauge = ({score}: EvalGaugeProps) => {
       position: "relative",
       paddingTop: "1em"
     }}>
-      <div style={{position: "absolute", width: "1.2em", height: "1.2em", background: "red", color: "#fff", padding: "2px", top: "0", left: `calc(${gaugePosition}% - 0.6em)`}}>{toCentipawn(score)}</div>
-      <div style={{width: "calc(100%/24)", background: "#ccc"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*2)", background: "#bbb"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*3)", background: "#aaa"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*4)", background: "#999"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*5)", background: "#888"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*6)", background: "#777"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*6)", background: "#666"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*5)", background: "#555"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*4)", background: "#444"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*3)", background: "#333"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*2)", background: "#222"}}>&nbsp;</div>
-      <div style={{width: "calc(100%/24*1)", background: "#111"}}>&nbsp;</div>
+      <div style={{ position: "absolute", width: "1.2em", height: "1.2em", background: "red", borderRadius: '0.6em', color: "#fff", padding: "2px", textAlign: 'center', transition: 'all 1s', verticalAlign: 'middle', top: "0", left: `calc(${gaugePosition}% - 0.6em)` }}>{toCentipawn(score)}</div>
+      <div style={{ width: "calc(100%/24)", background: "#ccc" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*2)", background: "#bbb" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*3)", background: "#aaa" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*4)", background: "#999" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*5)", background: "#888" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*6)", background: "#777" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*6)", background: "#666" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*5)", background: "#555" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*4)", background: "#444" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*3)", background: "#333" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*2)", background: "#222" }}>&nbsp;</div>
+      <div style={{ width: "calc(100%/24*1)", background: "#111" }}>&nbsp;</div>
     </div>
   )
 }
+
